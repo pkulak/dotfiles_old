c7241e7eae9907dae5c0493a645181ecd50163ed
Phil Kulak <pkulak@gmail.com>
2020-03-08 11:14:39 -0700

    Not a fan of that Spotify icon.

--- .config/waybar/modules/spotify.sh
+++ .config/waybar/modules/spotify.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 class=$(playerctl metadata --player=spotify --format '{{lc(status)}}')
-icon=""
 
 if [[ $class == "playing" ]]; then
   info=$(playerctl metadata --player=spotify --format '{{artist}} - {{title}}')
   if [[ ${#info} > 40 ]]; then
     info=$(echo $info | cut -c1-40)"..."
   fi
-  text=$info" "$icon
+  text=$info 
 elif [[ $class == "paused" ]]; then
-  text=$icon
+  text=""
 elif [[ $class == "stopped" ]]; then
   text=""
 fi
