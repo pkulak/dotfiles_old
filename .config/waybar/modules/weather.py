f5a273669c2ca5d0140ed9f2d8233ba40f0b76ce
Phil Kulak <pkulak@gmail.com>
2020-03-20 16:32:42 -0700

    Put high/low into the weather widget.

--- .config/waybar/modules/weather.py
+++ .config/waybar/modules/weather.py
@@ -3,9 +3,17 @@
 from lxml import html
 import requests
 
-resp = requests.get("https://darksky.net/forecast/45.5813,-122.6343/us12/en")
+lat = "45.58"
+lng = "-122.63"
+
+resp = requests.get("https://darksky.net/forecast/{},{}/us12/en".format(lat, lng))
 tree = html.fromstring(resp.content)
 
 summary = tree.cssselect("span.summary.swap")
+high = tree.cssselect("a[data-day=\"0\"] span.maxTemp")
+low = tree.cssselect("a[data-day=\"1\"] span.minTemp")
 
-print(summary[0].text_content())
+print(
+        summary[0].text_content(),
+        "↑" + high[0].text_content(),
+        "↓" + low[0].text_content())
