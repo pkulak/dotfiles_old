bb96b1df09841b27010cf28facbb668727519a0c
Phil Kulak <pkulak@gmail.com>
2020-05-10 16:38:46 -0700

    Start up all my windows how I like 'em.

---
+++ .config/sway/home-screen.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+swaymsg "exec alacritty"
+sleep 0.3s
+swaymsg "split vertical"
+swaymsg "exec alacritty"
+sleep 0.3s
+swaymsg "focus parent"
+swaymsg "exec firefox"
+sleep 0.5s
+swaymsg "resize grow width 25ppt"
