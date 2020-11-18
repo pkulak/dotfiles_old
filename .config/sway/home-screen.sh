be103aa86bc234cd9552e356691c4117fe02a995
Phil Kulak <pkulak@gmail.com>
2020-11-18 13:29:43 -0800

    Change how my desktop is set up.

--- .config/sway/home-screen.sh
+++ .config/sway/home-screen.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-swaymsg "exec QT_QPA_PLATFORM=wayland telegram-desktop"
-sleep 3s
-swaymsg "split vertical"
 swaymsg "exec alacritty"
 sleep 1s
+swaymsg "split vertical"
 swaymsg "focus parent"
 swaymsg "exec firefox"
 sleep 3s
 swaymsg "split vertical"
 swaymsg "layout tabbed"
 swaymsg "resize grow width 35ppt"
+swaymsg "workspace 10"
+swaymsg "exec signal-desktop"
 swaymsg "exec spotify"
-sleep 3s
-swaymsg "move scratchpad"
-sleep 3s
-playerctl play
+sleep 1s 
+swaymsg "workspace 1"
