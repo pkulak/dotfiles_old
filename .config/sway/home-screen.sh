d30b992104ee9574aa4f4a30a02651fadf1e3d78
Phil Kulak <pkulak@gmail.com>
2021-06-17 08:07:40 -0700

    Fix the home screen layout.

--- .config/sway/home-screen.sh
+++ .config/sway/home-screen.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 swaymsg "exec alacritty"
 sleep 1s
-swaymsg "split vertical"
-swaymsg "focus parent"
 swaymsg "exec firefox"
 sleep 3s
-swaymsg "split vertical"
 swaymsg "layout tabbed"
 swaymsg "resize grow width 35ppt"
 swaymsg "workspace 10"
 swaymsg "exec element-desktop"
 swaymsg "exec spotify"
-sleep 1s 
+sleep 3s 
 swaymsg "workspace 1"
