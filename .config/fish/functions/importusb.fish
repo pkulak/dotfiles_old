c6687ea93f0c17860acc906d7f5d612e05410a3d
Phil Kulak <pkulak@gmail.com>
2021-08-11 22:25:12 -0700

    New camera!

--- .config/fish/functions/importusb.fish
+++ .config/fish/functions/importusb.fish
@@ -1,5 +1,6 @@
 # Defined in /home/phil/.config/fish/config.fish @ line 63
 function importusb
   mkdir -p ~/Pictures/negatives/(date +%m)/
-  mv --backup=numbered /mnt/usbstick/DCIM/100OLYMP/* ~/Pictures/negatives/(date +%m)/
+  mv --backup=numbered /mnt/usbstick/DCIM/102OLYMP/*.ORF ~/Pictures/negatives/(date +%m)/
+  rm /mnt/usbstick/DCIM/102OLYMP/*.JPG 
 end
