b1f916f14ca9f34554047cba1a7d0b99592d9877
Phil Kulak <pkulak@gmail.com>
2021-08-22 12:36:36 -0700

    Video import and backup support.

--- .config/fish/functions/importusb.fish
+++ .config/fish/functions/importusb.fish
@@ -1,6 +1,14 @@
 # Defined in /home/phil/.config/fish/config.fish @ line 63
 function importusb
   mkdir -p ~/Pictures/negatives/(date +%m)/
-  mv --backup=numbered /mnt/usbstick/DCIM/102OLYMP/*.ORF ~/Pictures/negatives/(date +%m)/
-  rm /mnt/usbstick/DCIM/102OLYMP/*.JPG 
+  mkdir -p ~/Videos/negatives/(date +%m)/
+
+  set photos /mnt/usbstick/DCIM/100OLYMP/*.ORF
+  set videos /mnt/usbstick/DCIM/100OLYMP/*.MOV
+  set jpegs /mnt/usbstick/DCIM/100OLYMP/*.JPG
+
+  count $photos && mv --backup=numbered $photos ~/Pictures/negatives/(date +%m)/
+  count $videos && mv --backup=numbered $videos ~/Videos/negatives/(date +%m)/
+
+  count $jpegs && rm $jpegs
 end
