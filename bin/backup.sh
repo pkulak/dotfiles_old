b3cb32e54eb157c043b7fc83bbb3943262a58a3c
Phil Kulak <pkulak@gmail.com>
2021-08-22 12:36:36 -0700

    Video import and backup support.

--- bin/backup.sh
+++ bin/backup.sh
@@ -2,3 +2,4 @@
 
 rsync -aH --delete ~/Documents/scanned/ 3610@usw-s003.rsync.net:Scanned/2021/
 rsync -aH --delete ~/Pictures/negatives/ 3610@usw-s003.rsync.net:Negatives/2021/
+rsync -aH --delete ~/Videos/negatives/ 3610@usw-s003.rsync.net:Videos/Negatives/2021/
