ff3c831d43de7af637c6177d8205898cd3cd762e
Phil Kulak <pkulak@gmail.com>
2021-09-15 08:27:39 -0700

    History backup.

--- bin/backup.sh
+++ bin/backup.sh
@@ -3,3 +3,5 @@
 rsync -aH --delete ~/Documents/scanned/ 3610@usw-s003.rsync.net:Scanned/2021/
 rsync -aH --delete ~/Pictures/negatives/ 3610@usw-s003.rsync.net:Negatives/2021/
 rsync -aH --delete ~/Videos/negatives/ 3610@usw-s003.rsync.net:Videos/Negatives/2021/
+
+rsync -aH --delete ~/.local/share/fish 3610@usw-s003.rsync.net:.local/share/
