4808e4e08979e374e59835b2f887fb7909415cbb
Phil Kulak <pkulak@gmail.com>
2021-10-07 14:02:42 -0700

    Use sshfs normally.

--- .config/fish/functions/mountrsync.fish
+++ .config/fish/functions/mountrsync.fish
@@ -1,4 +1,4 @@
 # Defined in /home/phil/.config/fish/config.fish @ line 50
 function mountrsync
-  sshfs -o cache=yes -o kernel_cache -o cache_timeout=86400 -o auto_cache -o reconnect 3610@prio.usw-s003.rsync.net:/data2/home/3610 /home/phil/rsync -C
+  sshfs 3610@prio.usw-s003.rsync.net:/data2/home/3610 /home/phil/rsync -C
 end
