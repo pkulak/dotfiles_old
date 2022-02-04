76b36631864dd85829d2f5817a651f89b2a34d93
Phil Kulak <pkulak@gmail.com>
2022-02-04 08:44:53 -0800

    rsync alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,6 +15,7 @@ alias ts 'date -u +"%Y-%m-%dT%H:%M:%SZ"'
 alias gdm 'sudo systemctl start gdm'
 alias l 'exa --long --all --links --git --sort mod'
 alias bc 'bc -lq'
+alias rs 'rsync -avH --info=progress2'
 
 set PATH $PATH $HOME/bin $HOME/.cargo/bin $HOME/dotfiles/bin $HOME/vevo/rest $HOME/.local/bin $HOME/.local/share/gem/ruby/3.0.0/bin/
 setenv EDITOR vim
