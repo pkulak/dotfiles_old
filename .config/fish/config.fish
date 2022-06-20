083dd672a8201064870b57687143ab33167f7301
Phil Kulak <pkulak@gmail.com>
2022-06-20 13:49:49 -0700

    Some random changes.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,6 +16,7 @@ alias l 'exa --long --all --links --git --sort mod'
 alias bc 'bc -lq'
 alias rs 'rsync -avH --info=progress2'
 alias cleanup_packages 'set -x; sudo pacman -Rs (pacman -Qdtq)'
+alias dr 'dragon-drag-and-drop -a -x'
 
 set PATH $PATH $HOME/bin $HOME/.cargo/bin $HOME/dotfiles/bin $HOME/vevo/rest/bin $HOME/.local/bin $HOME/.local/share/gem/ruby/3.0.0/bin/
 setenv EDITOR vim
--- .config/sway/home-screen.sh
+++ .config/sway/home-screen.sh
@@ -5,8 +5,10 @@ sleep 2s
 swaymsg "splitv"
 swaymsg "layout tabbed"
 swaymsg "resize grow width 35ppt"
-swaymsg "workspace 10"
-swaymsg "exec firefox music.kulak.us"
+swaymsg "workspace 9"
+swaymsg "exec sublime-music"
+sleep 1s
 swaymsg "exec element-desktop"
 sleep 2s 
 swaymsg "workspace 1"
+swaymsg reload
