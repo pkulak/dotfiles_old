25c5555be9402fc5e080f388e5b75a5e391d5d17
Phil Kulak <phil@kulak.us>
2022-09-10 19:58:38 -0700

    Thinkpad X1

---
+++ .config/sway/home-screen-x1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+swaymsg "workspace 1"
+swaymsg "exec alacritty"
+sleep 2s
+swaymsg "exec flatpak run org.mozilla.firefox"
+swaymsg "exec flatpak run im.riot.Riot --enable-features=UseOzonePlatform --ozone-platform=wayland"
+sleep 2s
+swaymsg "splitv"
+swaymsg "layout tabbed"
+swaymsg "resize grow width 20ppt"
