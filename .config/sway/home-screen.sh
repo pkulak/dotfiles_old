fc7ae0bdd61faa5bc8dcd8e21ab5d78fafb878de
Phil <phil@kulak.us>
2022-09-15 21:57:48 -0700

    Fix the home screen a bit.

--- .config/sway/home-screen.sh
+++ .config/sway/home-screen.sh
@@ -2,13 +2,12 @@
 swaymsg "exec flatpak run org.mozilla.firefox"
 swaymsg "exec alacritty"
 swaymsg "exec flatpak run im.riot.Riot --enable-features=UseOzonePlatform --ozone-platform=wayland"
-sleep 2s
+sleep 3s
 swaymsg "splitv"
 swaymsg "layout tabbed"
 swaymsg "resize grow width 35ppt"
 swaymsg "workspace 9"
-swaymsg "exec flatpak run org.mozilla.firefox www.fastmail.com"
 swaymsg "exec distrobox-enter -n arch -- sublime-music"
-sleep 2s 
+sleep 3s 
 swaymsg "workspace 1"
 swaymsg reload
