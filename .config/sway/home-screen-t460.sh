0b96da45e28b25444efa5fd42b8f3ff55a0d3fbf
Phil Kulak <phil@kulak.us>
2022-10-05 09:28:17 -0700

    t460 changes

--- .config/sway/home-screen-t460.sh
+++ .config/sway/home-screen-t460.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-swaymsg "exec firefox"
+swaymsg "workspace 1"
 swaymsg "exec alacritty"
+sleep 2s
+swaymsg "exec flatpak run org.mozilla.firefox"
+swaymsg "exec flatpak run im.riot.Riot --enable-features=UseOzonePlatform --ozone-platform=wayland"
 sleep 2s
 swaymsg "splitv"
 swaymsg "layout tabbed"
-swaymsg "resize grow width 35ppt"
-swaymsg "workspace 9"
-swaymsg "exec spotify"
-swaymsg "exec element-desktop"
-sleep 2s 
-swaymsg "workspace 1"
+swaymsg "resize grow width 20ppt"
