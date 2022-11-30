d84059e236b912c4edce8c5f7bb75dad3220c6ee
Phil <phil@kulak.us>
2022-11-30 14:48:58 -0800

    Some bits and bobs.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,10 +7,8 @@ alias ... 'cd ../..'
 alias md 'mkdir -p'
 alias sm 'GDK_BACKEND=wayland smerge .'
 alias ssh 'TERM=xterm-256color command ssh'
-alias sd 'sudo shutdown now'
 alias c clear
 alias za zathura
-alias bc 'bc -l'
 alias ts 'date -u +"%Y-%m-%dT%H:%M:%SZ"'
 alias l 'exa --long --all --links --git --sort mod'
 alias bc 'bc -lq'
@@ -18,10 +16,14 @@ alias rs 'rsync -avH --info=progress2'
 alias cleanup_packages 'set -x; sudo pacman -Rs (pacman -Qdtq)'
 alias dr 'dragon-drop -a -x'
 
+alias mtvevo 'gocryptfs ~/nas/Drive/crypt/vevo ~/vevo/Notes'
+alias mtprivate 'gocryptfs ~/nas/Drive/crypt/private ~/private'
+
 alias podman 'distrobox-host-exec podman'
 alias flatpak 'distrobox-host-exec flatpak'
 alias snapper 'distrobox-host-exec snapper'
 alias rpm-ostree 'distrobox-host-exec rpm-ostree'
+alias firefox 'distrobox-host-exec firefox'
 
 set PATH $PATH $HOME/bin $HOME/.cargo/bin $HOME/dotfiles/bin $HOME/vevo/rest/bin $HOME/.local/bin
 setenv EDITOR vim
