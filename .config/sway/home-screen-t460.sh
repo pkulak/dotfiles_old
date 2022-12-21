8830b2fb3aac7ff723bf1d71fd5960cbddbbeefa
Phil Kulak <phil@kulak.us>
2022-12-21 09:16:28 -0800

    Fix a startup script.

--- .config/sway/home-screen-t460.sh
+++ .config/sway/home-screen-t460.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 swaymsg "workspace 1"
-swaymsg "exec alacritty"
-sleep 2s
+swaymsg "exec footclient distrobox enter arch -- /usr/bin/fish"
 swaymsg "exec flatpak run org.mozilla.firefox"
 swaymsg "exec flatpak run im.riot.Riot --enable-features=UseOzonePlatform --ozone-platform=wayland"
-sleep 2s
+sleep 3s
 swaymsg "splitv"
 swaymsg "layout tabbed"
 swaymsg "resize grow width 20ppt"
